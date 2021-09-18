e9be3d847bfb661de39581e29261706663167b02
Wellington Gadelha <contato.informeai@gmail.com>
2021-09-18 09:51:24 -0300

    add cli

---
+++ cmd/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/informeai/network"
+	"log"
+	"os"
+)
+
+//usage os params.
+var usage = `Network:
+Wrapper for network management on MacOS using go language.
+USAGE:
+network [PARAMS]
+PARAMS:
+	--help -h    help commands.
+	--scan -s    scaning by network wireless.
+	--info -i    info from current wireless.
+`
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println(usage)
+		os.Exit(0)
+	}
+	result, err := parseArgs(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+
+}
+
+//parseArgs execute parse of args.
+func parseArgs(args []string) (string, error) {
+	switch args[1] {
+	case "--help", "-h":
+		fmt.Println(usage)
+	case "--scan", "-s":
+		var resul string
+		a := network.NewAirport()
+		resul, err := a.GetScan()
+		if err != nil {
+			return "", err
+		}
+		return resul, nil
+	case "--info", "-i":
+		a := network.NewAirport()
+		resul, err := a.GetInfo()
+		if err != nil {
+			return "", err
+		}
+		return resul, nil
+	}
+	return usage, nil
+}
